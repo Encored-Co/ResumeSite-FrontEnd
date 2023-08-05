b22d4b9588ca19f9d47bc4159bbc3c157ad24e52
DARSHAN V <darshan1221u@gmail.com>
2023-08-05 14:15:32 +0530

    newResume

--- src/Coponents/ResumeBuilder/index.js
+++ src/Coponents/ResumeBuilder/index.js
@@ -61,126 +61,130 @@ function ResumeBuilder(){
 
     return(
         <>
-            <div className="LandingPage_MainDiv">
-                <div className="TextAlignments">
-                    <p className="fontBasics Title2">Craft your perfect career @ Encored to professional success!</p>
+            <div className='header'>
+                <p id='headiing' className='mainheader'>Craft Your Perfect Career @ Encored to Professional Success!</p>
+            </div>
+            <div className="AIresume">
+                <div className='Chatgptcol'>
+                    <p id='suggestion' className='chatgptborder'>
+                        <p className='aireport'>AI REPORT</p>
+                        The computer is one of modern science’s marvels. Its origins can be traced back to the mechanical calculating machine invented in 1834 by Charles Babbage, an English mathematician.
+                        Because of the lack of engineering skill at the time, the machine was unable to fully realise Babbage’s vision.
+                        Computers today are electronic. A computer can operate at high speeds because it is powered by a small current of electricity.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        It can perform calculations on very large numbers in a very short period of time using stored information and data.
+                        A computer can add, subtract, multiply, and divide large numbers in seconds. Even an expert mathematician would take much longer to complete such calculations.
+                        Computers are now widely used in all advanced countries for data processing, which includes fact compilation, correlation, and selection. The use of computers is increasing in all fields of research, industry, and business.
+                        Computers have numerous advantages. They save time, speed up work, save labour, and reduce costs in the long run. They are used in educational institutions, industries, research laboratories, government departments, and so on in all advanced countries.</p>
                 </div>
-                <div className="row">
-                    <div className="column InfoSide">
-                        <div className="TextAlignments FitWidth">
-                            <p className="fontBasics Title3">Welcome to</p>
-                            <p className="fontBasics Title1">Encored</p>
-                        </div>
-                        <div className="FitWidth enco">
-                            <img src={Logo} alt="Encored Logo"/>
-                        </div>
-                        <div className="InfoButtonDiv">
-                            <a href='https://www.encored.in' target='blank' className="LearnMore" name="Learn" id="LearnMore">Learn More</a>
-                        </div>
+                <div className='resumecol'>
+                    <div className="container" style={Display} ref={printRef}>
+                <div className="top">
+                    <div className="ImgCol">
+                        <img className='img1' alt="enco" src={Location.state.FileUrl}/>
                     </div>
-                    <div className="FormSide-Rp FormText column-Rp">
-                        <div className="container" style={Display} ref={printRef}>
-                            <div className="top">
-                                <div className="ImgCol">
-                                    <img className='img1' alt="enco" src={Location.state.FileUrl}/>
-                                </div>
-                                <div className="EmpCol">
-                                    <div className="profilealign" id="profilename">
-                                        <p id="name" className="MainName">{Location.state.Name}</p>
-                                        <p id="profiles" className="MainProf">{Location.state.Profession}</p>
-                                    </div>
+                    <div className="EmpCol">
+                        <div className="profilealign" id="profilename">
+                            <p id="name" className="MainName">{Location.state.Name}</p>
+                            <p id="profiles" className="MainProf">{Location.state.Profession}</p>
+                        </div>
 
-                                    <div className="summary">
-                                        <p className="linespace" id="profile"><b>PROFILE</b></p>
-                                        <p id="profiles" className="profilesz">{Location.state.Summary}</p>
-                                    </div>
-                                    <div className='Clear'></div>
-                                </div>
-                            </div>
-                            <div className="bottom">
-                                <div className="bottomleft">
-                                    <div>
-                                        <p id="name" className="linespace"><b>CONTACT</b></p>
-                                        <p id="profiles" className="profilesize"> <FaPhoneAlt className='SplSpace'/>{Location.state.Mobile}</p>
-                                        <p id="profiles" className="profilesize"> <GrMail className='SplSpace'/>{Location.state.Email}</p>
-                                        <p id="profiles" className="profilesize"> <FaLocationPin className="SplSpace"/>{Location.state.Address}</p>
-                                    </div>
-                                    <div>
-                                        <p id="name" className="linespace"><b>SKILLS</b></p>
-                                        <ul id="profiles" className="profilesize">
-                                            {
-                                                Location.state.Skills.trim().split(' ').map((key)=>{
-                                                    return(
-                                                    <li key={key}>{key}</li>)
-                                                })
-                                            }
-                                        </ul>
-                                    </div>
-                                    <div>
-                                        <p id="name" className="linespace"><b>EDUCATION</b></p>
-                                        {
-                                            Location.state.Education.map((key)=>{
-                                                return(
-                                                    <>
-                                                    <p id="profiles" className="profilesize" key={key.Major}><b>{key.Major}</b></p>
-                                                    <p id="profiles" className="profilesize" key={key.Ins}>{key.Ins}</p>
-                                                    <p id="profiles" className="profilesize" key={key.Loc}>{key.Loc}</p>
-                                                    </>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                </div>
-                                <div className="bottomright">
-                                    <div>
-                                        <p id="name" className="linespace"><b>WORK EXPERIENCE</b></p>
-                                        {
-                                            Location.state.Work.map((key)=>{
-                                                return(
-                                                    <>
-                                                    <p id="profiles" className="profilesize" key={key.Pos}><b>{key.Pos}</b></p>
-                                                    <p id="profiles" className="profilesize" key={key.Cmp}>{key.Cmp}</p>
-                                                    <p id="profiles" className="profilesize" key={key.Loc}>{key.Loc}</p>
-                                                    </>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="summary">
+                            <p className="linespace" id="profile"><b>PROFILE</b></p>
+                            <p id="profiles" className="profilesz">{Location.state.Summary}</p>
                         </div>
-                        <div >
-                            <p className='fontBasics m245'>*This is just a preview. Download to see results.</p>
-                            <p className='fontBasics War centerer mv'>Your Resume is Ready</p>
-                            <div>
-                                <button className="Navigator FloatL" name="Generate" onClick={()=>{
-                                    Navigate("/",{
-                                        state:{
-                                            Name:Location.state.Name,
-                                            Email:Location.state.Email,
-                                            Mobile:Location.state.Mobile,
-                                            Summary:Location.state.Summary,
-                                            Address:Location.state.Address,
-                                            Profession:Location.state.Profession,
-                                            Education:Location.state.Education,
-                                            Work:Location.state.Work,
-                                            Skills:Location.state.Skills,
-                                            File:Location.state.File,
-                                            FileUrl:Location.state.FileUrl
-                                        }
-                                    })
-                                }} id="Generate"><i className="fa-solid fa-circle-chevron-left NaviIcon"></i>Back</button>
-                            </div>
-                            <div>
-                                <button className="Navigator FloatR" onClick={()=>{downloadPdfDocument()}}>Download <i className="fa-solid fa-arrow-down NaviIcon"></i></button>
-                            </div>
-                            <div className='Clear'></div>
-                        </div>                        
+                        <div className='Clear'></div>
                     </div>
                 </div>
+                <div className="bottom">
+                    <div className="bottomleft">
+                        <div>
+                            <p id="name" className="linespace"><b>CONTACT</b></p>
+                            <p id="profiles" className="profilesize"> <FaPhoneAlt className='SplSpace'/>{Location.state.Mobile}</p>
+                            <p id="profiles" className="profilesize"> <GrMail className='SplSpace'/>{Location.state.Email}</p>
+                            <p id="profiles" className="profilesize"> <FaLocationPin className="SplSpace"/>{Location.state.Address}</p>
+                        </div>
+                        <div>
+                            <p id="name" className="linespace"><b>SKILLS</b></p>
+                            <ul id="profiles" className="profilesize">
+                                {
+                                    Location.state.Skills.trim().split(' ').map((key)=>{
+                                        return(
+                                        <li key={key}>{key}</li>)
+                                    })
+                                }
+                            </ul>
+                        </div>
+                        <div>
+                            <p id="name" className="linespace"><b>EDUCATION</b></p>
+                            {
+                                Location.state.Education.map((key)=>{
+                                    return(
+                                        <>
+                                        <p id="profiles" className="profilesize" key={key.Major}><b>{key.Major}</b></p>
+                                        <p id="profiles" className="profilesize" key={key.Ins}>{key.Ins}</p>
+                                        <p id="profiles" className="profilesize" key={key.Loc}>{key.Loc}</p>
+                                        </>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                    <div className="bottomright">
+                        <div>
+                            <p id="name" className="linespace"><b>WORK EXPERIENCE</b></p>
+                            {
+                                Location.state.Work.map((key)=>{
+                                    return(
+                                        <>
+                                        <p id="profiles" className="profilesize" key={key.Pos}><b>{key.Pos}</b></p>
+                                        <p id="profiles" className="profilesize" key={key.Cmp}>{key.Cmp}</p>
+                                        <p id="profiles" className="profilesize" key={key.Loc}>{key.Loc}</p>
+                                        </>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+                </div>
+            </div>
+            <div className='navigationcol'>
+                <div className='navigation_col1'>
+                    <button className="Navigator" name="Generate" onClick={()=>{
+                        Navigate("/",{
+                            state:{
+                                Name:Location.state.Name,
+                                Email:Location.state.Email,
+                                Mobile:Location.state.Mobile,
+                                Summary:Location.state.Summary,
+                                Address:Location.state.Address,
+                                Profession:Location.state.Profession,
+                                Education:Location.state.Education,
+                                Work:Location.state.Work,
+                                Skills:Location.state.Skills,
+                                File:Location.state.File,
+                                FileUrl:Location.state.FileUrl
+                            }
+                        })
+                    }} id="Generate"><i className="fa-solid fa-circle-chevron-left NaviIcon"></i>Back</button>
+                </div>
+                <div className='navigation_col2'>
+                    <button className="Navigator" onClick={()=>{downloadPdfDocument()}}>Download <i className="fa-solid fa-arrow-down NaviIcon"></i></button>
+                </div>
+                <div className='Clear'></div>                        
             </div>
         </>
     )
 }
-
 export default ResumeBuilder;
